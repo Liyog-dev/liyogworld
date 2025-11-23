799318bff7a042182aa38843a8c239b18e8b25a7
Liyog-dev <liyogbartoos@gmail.com>
2025-11-24 00:34:54 +0100

    Create tailwind.config.js

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html", "./assets/js/**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        prayerGreen: '#137333',
+        goldBorder: '#f9ab00'
+      }
+    }
+  },
+  plugins: [],
+}
